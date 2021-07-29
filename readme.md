c702270d2126f5316208ff3726f63953fa22b279
ishaanee28 <oreoishaanee28@gmail.com>
2021-07-29 18:07:21 +0530

    day 1

--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
-### To Do
-- 
-- 
-- 
+### To Do (29-07-21)
+- creating sprites
+    1. Girl sprite x
+    2. Zombie sprite
+    3. Bulb sprite
+    4. knife sprite
+    5. ground sprite x
+- added movement for ground sprite and girl sprite
+- girl falling down
--- sketch.js
+++ sketch.js
@@ -1,5 +1,22 @@
+var girl, ground;
+
 function setup(){
+    createCanvas(1000,600)
+    ground=createSprite(500,570,2000,2.7);
+    girl=createSprite(280,555,30,30);
+
 }
 
 function draw(){
+    background("black");
+    ground.velocityX=5;
+if(ground.x>1000){
+    ground.x=ground.width/2;
+}
+if(keyDown("space")){
+    girl.velocityY=-30;
+}
+girl.velocityY=girl.velocityY+0.5
+girl.collide(ground);
+    drawSprites();
 }
