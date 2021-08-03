bb3329a97374e1888fb85dcb7b3e5f6fb56ab913
ishaanee28 <oreoishaanee28@gmail.com>
2021-08-03 18:02:07 +0530

    day 2

--- sketch.js
+++ sketch.js
@@ -1,22 +1,35 @@
 var girl, ground;
+var zombie;
+var invisibleGround,zombieHolder;
+
+function preload(){
+    groundImage=loadImage("images/skyline.png")
+
+}
 
 function setup(){
-    createCanvas(1000,600)
-    ground=createSprite(500,570,2000,2.7);
-    girl=createSprite(280,555,30,30);
-
+    createCanvas(800,600)
+    ground=createSprite(400,500,2000,2.7);
+    ground.addImage(groundImage)
+    //ground.scale=0.5;
+    ground.x=ground.width/2
+    girl=createSprite(370,500,10,30);
+    zombie=createSprite(120,500,30,10)
+    zombieHolder=createSprite(120,530,30,20)
+    zombieHolder.shapeColor="yellow";
+    invisibleGround=createSprite(370,530,30,10)
 }
 
 function draw(){
     background("black");
     ground.velocityX=5;
-if(ground.x>1000){
-    ground.x=ground.width/2;
+if(ground.x>ground.width/2){
+    ground.x=0;
 }
 if(keyDown("space")){
     girl.velocityY=-30;
 }
 girl.velocityY=girl.velocityY+0.5
-girl.collide(ground);
+girl.collide(invisibleGround);
     drawSprites();
 }
