6b6ab246e227817fb22cb084955fe9e66cbcdadf
ishaanee28 <oreoishaanee28@gmail.com>
2021-08-05 17:59:36 +0530

    day 3

--- readme.md
+++ readme.md
@@ -1,9 +1,17 @@
 ### To Do (29-07-21)
 - creating sprites
     1. Girl sprite x
-    2. Zombie sprite
-    3. Bulb sprite
-    4. knife sprite
+    2. Zombie sprite x
+    3. Bulb sprite x
+    4. knife sprite x
     5. ground sprite x
-- added movement for ground sprite and girl sprite
-- girl falling down
+- added movement for ground sprite and girl sprite x
+- girl falling down x
+### To do 
+- add lives
+- add images
+- fix destroying all sprites
+- add emojis
+- find girl running animation(if possible)
+- scale knife,girl,bulb and zombie images
+- change the background color accordingly
--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 var girl, ground;
 var zombie;
 var invisibleGround,zombieHolder;
+var bulbGroup,knifeGroup;
+var score=0;
 
 function preload(){
     groundImage=loadImage("images/skyline.png")
-
+    
 }
 
 function setup(){
@@ -14,22 +16,49 @@ function setup(){
     //ground.scale=0.5;
     ground.x=ground.width/2
     girl=createSprite(370,500,10,30);
-    zombie=createSprite(120,500,30,10)
-    zombieHolder=createSprite(120,530,30,20)
-    zombieHolder.shapeColor="yellow";
+    zombie=createSprite(50,500,30,10)
     invisibleGround=createSprite(370,530,30,10)
+    bulbGroup=new Group();
+    knifeGroup=new Group();
 }
 
 function draw(){
     background("black");
+    textSize(24);
+    text("score : "+score,10,20)
     ground.velocityX=5;
 if(ground.x>ground.width/2){
     ground.x=0;
 }
 if(keyDown("space")){
-    girl.velocityY=-30;
+    girl.velocityY=-10;
+}
+spawnBulb();
+spawnKnife();
+if(girl.isTouching(bulbGroup)){
+    bulbGroup.destroyEach();
+    score=score+1 //score+=1
+
 }
 girl.velocityY=girl.velocityY+0.5
 girl.collide(invisibleGround);
     drawSprites();
+}
+function spawnBulb(){
+    if(frameCount%100==0){
+        var rand=Math.round(random(200,400))
+        var bulb=createSprite(800,rand,20,20)
+        bulb.velocityX=-5;
+        bulbGroup.add(bulb)
+    }
+   
+}
+function spawnKnife(){
+    if(frameCount%100==0){
+        var rand=Math.round(random(550,750))
+        var knife=createSprite(800,rand,20,20)
+        knife.velocityX=-5;
+        knifeGroup.add(knife)
+    }
+   
 }
